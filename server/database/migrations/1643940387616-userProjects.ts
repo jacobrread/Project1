d3c6b43a3efe8fed8802548de21a84b9f2a11dac
jacobread <jread8888@gmail.com>
2022-02-04 22:45:10 -0700

    I guess I didn't add all the files with my last commit

--- server/database/migrations/1643940387616-userProjects.ts
+++ server/database/migrations/1643940387616-userProjects.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class UserProjects1643940387616 implements MigrationInterface {
+export class userProjects1643940387616 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
